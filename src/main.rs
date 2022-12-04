3a6475a11b38558d8091de92d82a88619302c624
Todd Stellanova <tstellanova@users.noreply.github.com>
2022-12-04 15:04:35 -0800

    first simple plot drawing

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,21 @@ use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
 use panic_probe as _;
+use rp2040_hal as p_hal;
+use fugit::RateExtU32;
+
+use ssd1351::{
+    self,
+    properties::{DisplaySize, DisplayRotation},
+    interface::SpiInterface,
+    mode::{GraphicsMode, displaymode::DisplayModeTrait},
+
+};
+
+
+use embedded_graphics::{
+    pixelcolor::Rgb565, prelude::*, primitives::{Polyline, PrimitiveStyle},
+};
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
@@ -54,6 +69,49 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.led.into_push_pull_output();
+
+    // A "heartbeat" shaped polyline
+    let points: [Point; 10] = [
+	Point::new(10, 64),
+	Point::new(50, 64),
+	Point::new(60, 44),
+	Point::new(70, 64),
+	Point::new(80, 64),
+	Point::new(90, 74),
+	Point::new(100, 10),
+	Point::new(110, 84),
+	Point::new(120, 64),
+	Point::new(300, 64),
+    ];
+
+    let line_style = PrimitiveStyle::with_stroke(Rgb565::GREEN, 5);
+
+    let _spi0_sck_pin = pins.gpio6.into_mode::<p_hal::gpio::FunctionSpi>();
+    let _spi0_do_pin = pins.gpio7.into_mode::<p_hal::gpio::FunctionSpi>();
+    //let mut spi0_di_pin  = pins.gpio4.into_mode::<p_hal::gpio::FunctionSpi>();
+
+    let dc_pin  = pins.gpio8.into_push_pull_output();
+    //let mut cs_pin = pins.gpio9.into_push_pull_output();
+    let spi0 = p_hal::Spi::<_, _, 8>::new(pac.SPI0);
+
+    // Exchange the uninitialised SPI driver for an initialised one
+    let spi0 = spi0.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        16u32.MHz(),
+        &embedded_hal::spi::MODE_0,
+    );
+
+
+    let spii = SpiInterface::new(spi0, dc_pin );
+    let mut display_base = ssd1351::display::Display::new(spii, DisplaySize::Display128x128, DisplayRotation::Rotate0);
+    let _ = display_base.init();
+  
+    let mut display = GraphicsMode::new(display_base);
+
+    Polyline::new(&points)
+      .into_styled(line_style)
+      .draw(&mut display).unwrap();
 
     loop {
         info!("on!");
